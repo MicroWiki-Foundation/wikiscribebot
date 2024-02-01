baed8878ae732a1c3a5e1ce11b9cf980e0ca47c9
Karel Douda <kareldouda1@gmail.com>
2024-02-01 19:40:56 +0100

    Move cron implementation to userland

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
-const { mwn } = require('mwn');
 require('dotenv').config();
+const { CronJob } = require('cron');
+const { mwn } = require('mwn');
 
 const DeduplicateRequests = require('./tasks/drafts/deduplicate_requests');
 const UpdateDraftTemplates = require('./tasks/drafts/update_draft_templates');
@@ -13,28 +14,37 @@ globalThis.fetch = fetch;
     const bot = await mwn.init({
         apiUrl: process.env.API_URI,
     
-        // Can be skipped if the bot doesn't need to sign in
         username: process.env.BOT_USERNAME,
         password: process.env.BOT_PASSWORD,
     
-        // Set your user agent (required for WMF wikis, see https://meta.wikimedia.org/wiki/User-Agent_policy):
         userAgent: 'WikiScribeBot',
     
-        // Set default parameters to be sent to be included in every API request
         defaultParams: {
             assert: 'user' // ensure we're logged in
         }
     });
-
-    const tasks = [
-        new AutoRejectDrafts(bot),
-        new UpdateDraftTemplates(bot),
-        new DeduplicateRequests(bot),
-    ];
-
-    // todo - create task report for the Wiki?
-
-    for (let j of tasks) {
-        await j.run();
-    }
+    
+    const job = CronJob.from({
+        cronTime: '*/5 * * * *',
+        onTick: async function () {
+            console.log('Running tasks...');
+    
+            (async () => {
+                const tasks = [
+                    new AutoRejectDrafts(bot),
+                    new UpdateDraftTemplates(bot),
+                    new DeduplicateRequests(bot),
+                ];
+            
+                // todo - create task report for the Wiki?
+            
+                for (let j of tasks) {
+                    await j.run();
+                }
+            })();
+        },
+        start: true,
+        timeZone: 'UTC'
+    });
 })();
+
